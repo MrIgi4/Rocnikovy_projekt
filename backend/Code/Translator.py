e52a47dda10962f80c44719d435e01525a9b1cec
Igor <travenec2@uniba.sk>
2026-01-16 22:32:21 +0100

    added support for augmented assign (x += 1), else, else if, for and while

--- backend/Code/Translator.py
+++ backend/Code/Translator.py
@@ -9,7 +9,7 @@ class Translator(ast.NodeVisitor):
         self.abstractSyntaxTree = None
         self.cppCode = Translation.Translation()
         self.variables = {}
-        self.indentVariables: list[list[str]] = [] #list of variables by indentation for deletion after their scope is exited
+        self.scopedVariables: list[list[str]] = [] #list of variables by indentation for deletion after their scope is exited
         self.imports = set()
         self.indentation = 0
 
@@ -64,7 +64,7 @@ class Translator(ast.NodeVisitor):
             return self.createValue(value.left) + " " + self.createValue(value.ops[0]) + " " + self.createValue(value.comparators[0])
         elif isinstance(value, ast.Add):
             return "+"
-        elif isinstance(value, ast.Sub):
+        elif isinstance(value, ast.Sub) or isinstance(value, ast.USub):
             return "-"
         elif isinstance(value, ast.Mult):
             return "*"
@@ -110,14 +110,14 @@ class Translator(ast.NodeVisitor):
         elif isinstance(value, ast.List):
             self.imports.add("vector")
             if len(value.elts) == 0:
-                return "vector<auto>"
+                return "vector<auto>()"
 
             first_type = self.createTypeAnnotation(value.elts[0])
             for elt in value.elts[1:]:
                 if self.createTypeAnnotation(elt) != first_type:
-                    return "vector<auto>"
+                    return "vector<auto>()"
 
-            return "vector<" + first_type + ">"
+            return "vector<" + first_type + ">()"
         #todo low priority
         elif isinstance(value, ast.Tuple):
             self.imports.add("tuple")
@@ -135,17 +135,23 @@ class Translator(ast.NodeVisitor):
         #todo - inaccurate - makes entire line an assignment even if it contains e.g. calls
         self.cppCode.addCodeElement("    " * self.indentation + code, codeType)
 
-    def assignTarget(self, target, variableType: str) -> None:
+    def assignTarget(self, target, variableType: str, isReassignment: bool) -> None:
         self.variables[target.id] = variableType
-        if len(self.indentVariables) <= self.indentation:
-            self.indentVariables.append([])
-        self.indentVariables[self.indentation].append(target.id)
+        if len(self.scopedVariables) <= self.indentation:
+            self.scopedVariables.append([])
+        if not isReassignment:
+            self.scopedVariables[self.indentation].append(target.id)
 
     #@Override of functions from ast module
     def visit_Assign(self, node) -> None:
         variableType = self.createTypeAnnotation(node.value)
 
-        if  self.variables.keys().__contains__(node.targets[0].id):
+        if isinstance(node.targets[0], ast.Name):
+            isReassignment = self.variables.keys().__contains__(node.targets[0].id)
+        else:
+            isReassignment = False
+
+        if isReassignment:
             translation = ""
         else:
             translation = variableType + ' '
@@ -154,12 +160,12 @@ class Translator(ast.NodeVisitor):
         for target in node.targets:
             if isinstance(target, ast.Name):
                 names.append(target.id)
-                self.assignTarget(target, variableType)
+                self.assignTarget(target, variableType, isReassignment)
             elif isinstance(target, ast.Tuple):
                 for element in target.elts:
                     if isinstance(element, ast.Name):
                         names.append(element.id)
-                        self.assignTarget(element, variableType)
+                        self.assignTarget(element, variableType, isReassignment)
 
         variables = ""
         for i in range(len(names)):
@@ -173,20 +179,104 @@ class Translator(ast.NodeVisitor):
         self.emitCode(translation, "assignment")
         self.addSemicolon()
 
+    def visit_AugAssign(self, node) -> None:
+        translation = node.target.id
+        translation += " " + self.createValue(node.op) + "="
+        translation += " " + self.createValue(node.value)
+        self.emitCode(translation, "augment assignment")
+        self.addSemicolon()
+
+    def enterBlock(self):
+        if len(self.scopedVariables) <= self.indentation:
+            self.scopedVariables.append([])
+        self.indentation += 1
+
     def exitBlock(self):
-        for variable in self.indentVariables[self.indentation]:
+        for variable in self.scopedVariables[self.indentation]:
             self.variables.pop(variable)
-        self.indentVariables.pop(self.indentation - 1)
+        self.scopedVariables.pop(self.indentation - 1)
         self.indentation -= 1
 
     def visit_If(self, node) -> None:
         expression = self.createValue(node.test)
         self.emitCode("if (" + expression + ") {\n", "if")
-        self.indentation += 1
+        self.enterBlock()
         for stmt in node.body:
             self.visit(stmt)
         self.exitBlock()
-        self.emitCode("}\n", "endbracket")
+        self.emitCode("}\n", "endbracket - if")
+
+        if node.orelse:
+            if len(node.orelse) == 1 and isinstance(node.orelse[0], ast.If):
+                self.emitCode("else ", "elseif")
+                self.visit_If(node.orelse[0])  # recursive elif
+            else:
+                self.emitCode("else {\n", "else")
+                self.enterBlock()
+                for stmt in node.orelse:
+                    self.visit(stmt)
+                self.exitBlock()
+                self.emitCode("}\n", "endbracket - else or elseif")
+
+    def handleRange(self, args, newVariable : str) -> None:
+        start = 0
+        step = 1
+        if len(args) > 1:
+            start = self.createValue(args[0])
+            end = self.createValue(args[1])
+        else:
+            end = self.createValue(args[0])
+        if len(args) > 2:
+            step = self.createValue(args[2])
+        if step == 1:
+            stepIncrease = newVariable + "++"
+        else:
+            stepIncrease = newVariable + " += " + step
+        self.emitCode("for (int " + newVariable + " = " + start + ";" +
+                      " " + newVariable + " < " + end + ";" +
+                      " " + stepIncrease +
+                      ") {\n", "for")
+
+    def handleLen(self, args, newVariable : str) -> None:
+        self.emitCode("for (int " + newVariable + " = 0;" +
+                      " " + newVariable + " < " + self.createValue(args[0]) + ";" +
+                      " " + newVariable + "++) {\n", "for")
+
+    def visit_For(self, node) -> None:
+        newVariable = self.createValue(node.target)
+        iterable = node.iter
+        self.variables[newVariable] = self.createTypeAnnotation(node.target)
+        self.scopedVariables.append([])
+
+        if (isinstance(iterable, ast.Call) and
+              isinstance(iterable.func, ast.Name) and
+              iterable.func.id == "range"):
+            self.handleRange(iterable.args, newVariable)
+        elif (isinstance(iterable, ast.Call) and
+              isinstance(iterable.func, ast.Name) and
+              iterable.func.id == "len"):
+            self.handleLen(iterable.args, newVariable)
+        else:
+            #I expect it to be of form "iterable<type>"
+            iterableType = self.createTypeAnnotation(iterable)
+            newVariableType = iterableType[iterableType.find("<") + 1:-1]
+            self.emitCode("for (" + newVariableType + " " + newVariable + " : " +
+                          self.createValue(iterable) + ") {\n", "for")
+
+        self.enterBlock()
+        for stmt in node.body:
+            self.visit(stmt)
+        self.exitBlock()
+        self.emitCode("}\n", "endbracket - for")
+
+    def visit_While(self, node) -> None:
+        expression = self.createValue(node.test)
+        self.emitCode("while (" + expression + ") {\n", "while")
+        self.enterBlock()
+        for stmt in node.body:
+            self.visit(stmt)
+        self.exitBlock()
+        self.emitCode("}\n", "endbracket - while")
 
     def translate(self, text: str) -> None:
         #todo Syntax Error handling
--- backend/TestCases/TranslatorTests.py
+++ backend/TestCases/TranslatorTests.py
@@ -38,3 +38,59 @@ def test_basic_if():
                                        "if (x == 23) {\n"
                                        "    x = x + 1;\n"
                                        "}\n")
+
+def test_2_for_loops():
+    translator = Translator.Translator()
+    translator.translate("x = 23\n"
+              "y = [1, 2, 3]\n"
+              "for i in range(1, x):\n"
+              "    x = x + 1\n"
+              "for i in y:\n"
+              "    x += 1\n")
+    translator.printCode()
+
+    assert translator.cppCode.code == ("int x = 23;\n"
+                                       "vector<int> y = vector<int>();\n"
+                                       "for (int i = 1; i < x; i++) {\n"
+                                       "    x = x + 1;\n"
+                                       "}\n"
+                                       "for (int i : y) {\n"
+                                       "    x += 1;\n"
+                                       "}\n")
+
+def test_else_ifs():
+    translator = Translator.Translator()
+    translator.translate("if true:\n"
+                         "    x = -12\n"
+                         "if true:\n"
+                         "    x = 5\n"
+                         "elif false:\n"
+                         "    x = -1\n"
+                         "else:\n"
+                         "    x = 3\n")
+    translator.printCode()
+
+    assert translator.cppCode.code == ("if (true) {\n"
+"    int x = -12;\n"
+"}\n"
+"if (true) {\n"
+"    int x = 5;\n"
+"}\n"
+"else if (false) {\n"
+"    int x = -1;\n"
+"}\n"
+"else {\n"
+"    int x = 3;\n"
+"}\n")
+
+def test_while():
+    translator = Translator.Translator()
+    translator.translate("x = 0\n"
+              "while(true):\n"
+              "    x = x + 1\n")
+    translator.printCode()
+
+    assert translator.cppCode.code == ("int x = 0;\n"
+"while (true) {\n"
+"    x = x + 1;\n"
+"}\n")
